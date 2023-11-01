a96ec0180bbc5e52880a3163756f378d3213c051
shnizzedy <jon.clucas@childmind.org>
2023-11-01 18:58:25 +0000

    Deploying to gh-pages from @ shnizzedy/standards-checklist@deeab404238ca5a4f6f00304983fba0d23ea127a 🚀

--- app/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('nmind link', () => {
-  render(<App />);
-  const linkElement = screen.getByText("NMIND Coding Standards Checklist");
-  expect(linkElement).toBeInTheDocument();
-});
